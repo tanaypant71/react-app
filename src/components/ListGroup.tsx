89ef73495fea87853d5d9bd9a9461e4b39c1a532
TANAY PANT <tanaypant71@gmail.com>
2023-08-21 21:49:44 +0530

    final commit till conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,18 @@
 function ListGroup() {
-  const listGroupItems = [
+  let listGroupItems = [
     "Cras justo odio",
     "Dapibus ac facilisis in",
     "Morbi leo risus",
     "Porta ac consectetur ac",
   ];
 
+  listGroupItems = [];
+
   return (
     <>
       <h1>List</h1>
+      {/* with braces we can render anything dynamically */}
+      {listGroupItems.length === 0 && <p>There are no items in the list</p>}
       <ul className="list-group">
         {listGroupItems.map((listGroupItems) => (
           <li key={listGroupItems}>{listGroupItems}</li>
